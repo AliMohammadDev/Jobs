d330789272fd69e7464a56374f16ff5573bc699a
alishadel2001 <alishadel2001@gmail.com>
2025-10-31 00:40:36 +0300

    edit in tage url in index

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['tag','size' => 'base'])
+@props(['tag', 'size' => 'base'])
 
 @php
     $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
@@ -11,4 +11,4 @@
 @endphp
 
 
-<a href="/tags/{{ strtolower($tag->name) }}" class='{{ $classes }}'>{{ $tag->name }}</a>
+<a href="#" class='{{ $classes }}'>{{ $tag->name }}</a>
